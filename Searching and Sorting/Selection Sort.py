93a188ff12269e6519c8ff05ec007680167f4de1
Pranav <prnvsingaria.44@gmail.com>
2023-07-31 21:32:57 +0530

    selection sort

---
+++ Searching and Sorting/Selection Sort.py
@@ -0,0 +1,28 @@
+"""Selection Sort
+It works by repeatedly selecting the smallest (or largest) element from unsorted portion of the list and moving it to the sorted portion of the list.
+swapping it with first element of unsorted part.
+Advantages
+    it never makes more than O(n) swaps and can be useful when memory writing is costly.
+    works well with small datasets.
+    simple and easy to understand.
+Disadvantages
+    Does not preserve relative order of items with equal keys which means it is not stable.
+"""
+
+class Solution():
+
+    def selectionSort(self, arr):
+        for i in range(len(arr)):
+            min_idx = i
+            for j in range(i+1,len(arr)):
+                if arr[min_idx]>arr[j]:
+                    min_idx = j
+
+            arr[i],arr[min_idx]=arr[min_idx],arr[i]
+
+        return arr
+
+arr = [64, 25, 12, 22, 11]
+sol =Solution()
+ans = sol.selectionSort(arr)
+print(ans)
