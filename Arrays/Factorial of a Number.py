d2fca7959b100c3de8395f04a40558b7cc6a65f0
Pranav <prnvsingaria.44@gmail.com>
2023-07-31 13:47:33 +0530

    factorial for bigger number

--- Arrays/Factorial of a Number.py
+++ Arrays/Factorial of a Number.py
@@ -1,3 +1,5 @@
+import sys
+
 class Solution():
 
     def factorial(self,num):        #recursion
@@ -14,6 +16,40 @@ class Solution():
     def fac(self,n):                #one liner
         return 1 if (n==1 or n==0) else n*self.fac(n-1)
     
+    def largeNumFactorial(self, num): 
+                                        #for larger numbers
+        def multiply(n,res,res_size):
+            carry = 0
+            i=0
+            while i < res_size:
+                prod = n*res[i]+carry
+                carry = prod//10
+                res[i]= prod%10
+                i+=1
+
+            while carry:
+                res[res_size]= carry%10
+                carry //= 10
+                res_size += 1
+
+            return res_size
+                
+        res = [None]*500
+        res[0],res_size=1,1
+        x=2
+        while x<=num:
+            res_size =multiply(x,res,res_size)
+            x+=1
+       
+        i = res_size-1
+        while i >= 0:
+            sys.stdout.write(str(res[i]))
+            sys.stdout.flush()
+            i = i - 1
+        
+    
 sol =Solution()
 ans = sol.fac(6)
 print(ans)
+
+sol.largeNumFactorial(200)
