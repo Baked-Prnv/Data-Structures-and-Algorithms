81f7104513e614e93e2967b6c4f2d1d1a98e7497
Pranav <prnvsingaria.44@gmail.com>
2023-07-31 20:52:36 +0530

    insertion sort

---
+++ Searching and Sorting/Insertion Sort.py
@@ -0,0 +1,28 @@
+"""Insertion Sort
+Array is virtually split into sorted and unsorted part.
+Value from unsorted part is picked and placed at the correct position in the sorted part.
+Advantages:
+    Efficient for the small data values
+    Adaptive in nature, i.e., appropriate for datasets that are already partially sorted."""
+
+class Solution():
+
+    def insertionSort(self, arr):
+        
+        for i in range(1, len(arr)):
+            key = arr[i]
+
+            j = i-1
+            while j>=0 and key < arr[j]:
+                arr[j+1] = arr[j]
+                j -= 1
+            
+            arr[j+1] = key
+
+        return arr
+
+arr = [12,39,24,2,7,22, 11, 13, 5, 6]
+sol = Solution()
+ans = sol.insertionSort(arr)
+print(ans)
+
